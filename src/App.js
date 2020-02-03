f53dae53285bda61d8a2e065db9c2f0ceb1e7c92
Massimo Necchi <massimo.necchi@integrate.com>
2020-02-03 15:16:13 +0000

    fix error

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Pull Request
+          Learn React
         </a>
       </header>
     </div>
